3d997e0f96c9cac6bc65e03fb5b2de960d4b9be4
Ivander <ivandersalvadorruiz@gmail.com>
2025-01-08 09:39:32 -0300

    feat: created conceptual todo app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        loadComponent: async () => {
+            const m = await import('./home/home.component');
+            return m.HomeComponent;
+        },
+    },
+    {
+        path: 'todos',
+        loadComponent: async () => {
+            const m = await import('./todos/todos.component');
+            return m.TodosComponent;
+        },
+    }
+];
